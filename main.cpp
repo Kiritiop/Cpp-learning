492f440fad86f0188f1846c976b008bfb2277973
Kiritiop <wzirui102348@gmail.com>
2025-07-06 22:53:16 -0400

    Day 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main(){
     std::cout << "Hello World!" << std::endl;
+    std::string num;
+    std::cin >> num;
+    std::cout << "Your number is " << num << "!" << std::endl;
+    
+
     return 0;
 }
