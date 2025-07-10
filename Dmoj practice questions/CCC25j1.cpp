80d77c863803357bdb109cebaf9e48072cfb3697
Kiritiop <wzirui102348@gmail.com>
2025-07-09 22:28:47 -0400

    j1

---
+++ Dmoj practice questions/CCC25j1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int c;
+    cin >> c;
+    int p;
+    cin >> p;
+    if (c * p > n) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+
+
+}
