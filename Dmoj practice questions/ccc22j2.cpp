6a1f9f431e3694d691ee4610e9e344335ec8f92c
Kiritiop <wzirui102348@gmail.com>
2025-07-31 11:57:48 -0400

    22j2

--- Dmoj practice questions/ccc22j2.cpp
+++ Dmoj practice questions/ccc22j2.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main()
 {
-
+    int n = 0;
+    int pt, foul;
+    int star = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pt >> foul;
+        if ((pt * 5 - foul * 3) > 40)
+        {
+            star++;
+        }
+    }
+    if (star == n)
+    {
+        cout << star << "+" << endl;
+    }
+    else
+    {
+        cout << star << endl;
+    }
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 int main()
 {
-    int r, s;
-    cin >> r >> s;
-    cout << r * 8 + s * 3 - 28 << endl;
+    int n = 0;
+    int pt, foul;
+    int star = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pt >> foul;
+        if ((pt * 5 - foul * 3) > 40)
+        {
+            star++;
+        }
+    }
+    if (star == n)
+    {
+        cout << star << "+" << endl;
+    }
+    else
+    {
+        cout << star << endl;
+    }
     return 0;
 }
